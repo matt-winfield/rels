bf9a897dbfe8b57a0381f57fcf9648f79a7da99e
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2023-10-21 18:02:28 +0100

    Only show each commit in one release

--- src/main.rs
+++ src/main.rs
@@ -99,15 +99,25 @@ fn commit_is_within_duration(commit: &Commit, max_age: std::time::Duration) -> b
     true
 }
 
+struct CommitTagInfo<'a> {
+    commit: Commit<'a>,
+    depth: usize,
+    tag_name: String,
+}
+
 fn main() {
     let args = Args::parse();
     let repo = get_repo();
 
     let max_age = duration_str::parse(&args.age).unwrap_or_default();
 
+    let mut commit_to_tag: HashMap<String, CommitTagInfo> = HashMap::new();
+    let mut tag_names = Vec::new();
+
     let _ = repo.tag_foreach(|tag_id, _| {
         let tag = repo.find_tag(tag_id).unwrap();
-        println!("{}", tag.name().unwrap_or_default().green());
+        let tag_name = tag.name().unwrap_or_default().to_owned();
+        tag_names.push(tag_name.clone());
 
         if let Ok(target) = tag.target() {
             if let Ok(commit) = repo.find_commit(target.id()) {
@@ -115,14 +125,67 @@ fn main() {
                     return true;
                 }
 
-                println!("  {}", commit.message().unwrap_or_default().trim());
+                // Add the commit directly referenced by the tag
+                commit_to_tag.insert(
+                    commit.id().to_string(),
+                    CommitTagInfo {
+                        commit: commit.clone(),
+                        depth: 0,
+                        tag_name: tag_name.clone(),
+                    },
+                );
+
                 let parents = get_parent_commits(&repo, commit, args.depth);
                 for parent in parents {
-                    println!("  {}", parent.commit.message().unwrap_or_default().trim());
+                    let parent_id = parent.commit.id().to_string();
+                    let parent_depth = parent.depth;
+
+                    if let Some(existing) = commit_to_tag.get(&parent_id) {
+                        if existing.depth < parent_depth {
+                            continue;
+                        }
+                    }
+
+                    commit_to_tag.insert(
+                        parent_id,
+                        CommitTagInfo {
+                            commit: parent.commit,
+                            depth: parent_depth,
+                            tag_name: tag_name.clone(),
+                        },
+                    );
                 }
             }
         }
-
         true
     });
+
+    let tag_to_commits = commit_to_tag
+        .iter()
+        .fold(HashMap::new(), |mut map, (_, info)| {
+            map.entry(&info.tag_name)
+                .or_insert_with(Vec::new)
+                .push(info);
+            map
+        });
+
+    tag_names.sort();
+
+    for tag_name in tag_names {
+        let empty = Vec::new();
+        let commits = tag_to_commits.get(&tag_name).unwrap_or(&empty);
+
+        match commits.is_empty() {
+            true => {
+                println!("{}", format!("{} (no unique commits)", tag_name).dimmed())
+            }
+            false => {
+                println!("{}", tag_name.green().bold())
+            }
+        }
+
+        for commit in commits {
+            println!("  {}", commit.commit.summary().unwrap_or_default());
+        }
+    }
 }
